e109a56a9282ba6e4193c29e6f458cd98432f3e1
jakemeinershagen <jakemeinershagen@gmail.com>
2024-12-28 08:45:50 -0600

    statemachine should be good
    just need to test

--- scripts/state.gd
+++ scripts/state.gd
@@ -1,22 +1,29 @@
 extends Node
 class_name State
 
+## animation that should be played by this script
+@export var animation_name: String
+## names of states that this state is allowed to transition to
+@export var allowed_transitions: Array[String]
+@export var disable_warnings: bool = false
 
 # these two should be set by the state_machine
 var animation: AnimatedSprite2D
 var player: CharacterBody2D
 
-@export var animation_name: String
-
+var lock = false
 
 func _ready() -> void:
 	# if no animation_name is set it will use the node name
 	if not animation_name:
 		animation_name = name.to_lower()
-		push_warning(
-			"{name} has no animation_name defined, setting animation_name to {name_lower}"
-			.format({"name": name, "name_lower": name.to_lower()})
-		)
+		if not disable_warnings:
+			push_warning(
+				"{name} has no animation_name defined, setting animation_name to {name_lower}"
+				.format({"name": name, "name_lower": name.to_lower()})
+			)
+	if not allowed_transitions and not disable_warnings:
+		push_warning("Allowed Transitions list is empty. This will allow tranistion to any state from this one.")
 
 
 func _enter_state():
--- scripts/state_machine.gd
+++ scripts/state_machine.gd
@@ -1,12 +1,13 @@
 extends Node
 class_name StateMachine
 
-
 @export var player: CharacterBody2D
 @export var animation: AnimatedSprite2D
+@export var start_state: State
 @export var disable_warnings: bool = false
 
 var states: Dictionary = {}
+var curr_state: State
 
 
 func register_states():
@@ -25,3 +26,25 @@ func register_states():
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	register_states()
+	
+	assert(start_state, "No start state provided.")
+	curr_state = states[start_state.name.to_lower()]
+
+
+## changes state to target, if state is in allowed transitions and lock is not true
+func change_state(target_state: String) -> bool:
+	if target_state not in states:
+		return false
+	if curr_state.lock:
+		return false
+	if (len(curr_state.allowed_transitions) > 0
+		and target_state not in curr_state.allowed_transitions):
+		return false
+	curr_state = states[target_state]
+	return true
+
+
+## this isn't implemented yet because I haven't needed it.
+func force_change_state():
+	## this would be something to use only if you really mean it.
+	assert(false, "this isn't implemented yet because I haven't needed it.")
